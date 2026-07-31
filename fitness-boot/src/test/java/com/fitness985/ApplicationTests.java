e9f4eae4c582efa8a8a193f204cf289d611a652f
NOMAD\25090 <2509094405@qq.com>
2026-07-31 17:23:49 +0800

    feat: 创建user和boot启动类模块，完善相关基础配置和父子模块间依赖问题

---
+++ fitness-boot/src/test/java/com/fitness985/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness985;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ fitness-user/src/test/java/com/fitnessuser/FitnessUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
