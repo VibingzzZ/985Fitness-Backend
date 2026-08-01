bb9662a4d435d90716c2e28aa79d0dda9f8e1eb1
NOMAD\25090 <2509094405@qq.com>
2026-08-01 15:29:56 +0800

    fix: 修复相关文件

--- fitness-common/src/test/java/com/fitness985/fitnesscommon/FitnessCommonApplicationTests.java
+++ fitness-common/src/test/java/com/fitness985/fitnesscommon/FitnessCommonApplicationTests.java
@@ -1,9 +1,7 @@
 package com.fitness985.fitnesscommon;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FitnessCommonApplicationTests {
 
     @Test
--- fitness-security/src/test/java/com/fitness985/fitnesssecurity/FitnessSecurityApplicationTests.java
+++ fitness-security/src/test/java/com/fitness985/fitnesssecurity/FitnessSecurityApplicationTests.java
@@ -1,9 +1,7 @@
 package com.fitness985.fitnesssecurity;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FitnessSecurityApplicationTests {
 
     @Test
